8b71375b13a1a8e9b97143e720e3bf99cedea089
Rustam Gasanov <rustamagasanov@gmail.com>
2014-12-31 01:49:42 +0700

    [+] added first router and controller actions

--- source_code/js/bookstore.js
+++ source_code/js/bookstore.js
@@ -1,1 +1,15 @@
 var bookStoreApp = new Backbone.Marionette.Application();
+
+var BookStoreRouter = Backbone.Marionette.AppRouter.extend({
+  controller:BookStoreController,
+    appRoutes:{
+￼￼￼￼￼ "": "displayBooks"
+    }
+});
+
+var BookStoreController = Backbone.Marionette.Controller.extend({
+  displayBooks:function(){
+    console.log("I will display books...");
+  }
+});
+
