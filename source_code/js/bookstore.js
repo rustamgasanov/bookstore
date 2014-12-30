b52c2d6751e7bdfa20a75f7286585c534133939f
Rustam Gasanov <rustamagasanov@gmail.com>
2014-12-31 02:00:16 +0700

    [+] added first controller/router, initializers

--- source_code/js/bookstore.js
+++ source_code/js/bookstore.js
@@ -1,15 +1,30 @@
-var bookStoreApp = new Backbone.Marionette.Application();
-
-var BookStoreRouter = Backbone.Marionette.AppRouter.extend({
-  controller:BookStoreController,
-    appRoutes:{
-￼￼￼￼￼ "": "displayBooks"
-    }
-});
+var BookStoreApp = new Backbone.Marionette.Application();
 
 var BookStoreController = Backbone.Marionette.Controller.extend({
-  displayBooks:function(){
+  displayBooks: function() {
     console.log("I will display books...");
   }
 });
 
+var BookStoreRouter = Backbone.Marionette.AppRouter.extend({
+  controller: BookStoreController,
+    appRoutes: {
+￼￼￼￼￼ "": "displayBooks"
+    }
+});
+
+BookStoreApp.addInitializer(function () {
+  var bookStoreController = new BookStoreController({
+    var bookStoreRouter = new BookStoreRouter({controller: controller});
+    console.log('Message from the addInitializer Method');
+  });
+})
+
+BookStoreApp.on('initialize:after', function () {
+  if (Backbone.history) {
+    Backbone.history.start();
+  }
+  console.log('Mesagge from initialize:after method');
+});
+
+BookStoreApp.start();
