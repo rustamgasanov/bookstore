44001209f5cf163562565d598b1d9b312d16bd9c
Rustam Gasanov <rustamagasanov@gmail.com>
2015-02-01 20:13:19 +0200

    [+] extracted region manager into example

---
+++ source_code/js/example_5_region_manager.js
@@ -0,0 +1,84 @@
+var BookStoreApp = new Marionette.Application();
+BookStoreApp.addRegions({
+  mainRegion: "#application"
+})
+
+var BookStoreController = Marionette.Controller.extend({
+  displayBooks: function() {
+    console.log('I will display books...');
+  }
+});
+
+var BookStoreRouter = Marionette.AppRouter.extend({
+  appRoutes: {
+    '': 'displayBooks'
+  }
+});
+
+var CategoryModel = Backbone.Model.extend({
+  defaults: {
+    name: '',
+    booksOnCategory: 0
+  }
+})
+
+var CategoriesCollection = Backbone.Collection.extend({
+  model: CategoryModel
+})
+
+var CategoryView = Marionette.ItemView.extend({
+  tagName: 'li',
+  template: '#category-template'
+})
+
+var CategoriesView = Marionette.CollectionView.extend({
+  tagName: 'ul',
+  className: 'unstyled',
+  childView: CategoryView
+})
+
+var CatalogLayoutView = Marionette.LayoutView.extend({
+  template: '#catalog-layout',
+  regions: {
+    categories: '#categories',
+    products: '#products',
+    order: '#order',
+    book: '#book'
+  }
+})
+
+BookStoreApp.addInitializer(function () {
+  var controller = new BookStoreController();
+  var router = new BookStoreRouter({controller: controller});
+  console.log('Message from the addInitializer Method');
+})
+
+BookStoreApp.on('start', function () {
+  if (Backbone.history) {
+    Backbone.history.start();
+  }
+  console.log('Message from initialize:after method');
+});
+
+BookStoreApp.start();
+
+$(function() {
+  var catalogLayoutView = new CatalogLayoutView();
+  BookStoreApp.mainRegion.show(catalogLayoutView);
+  var categoriesCollection = new CategoriesCollection([
+    { name: 'category1', booksOnCategory: 5 },
+    { name: 'category2', booksOnCategory: 10 }
+  ]);
+  catalogLayoutView.categories.show(new CategoriesView(
+      { collection: categoriesCollection }
+    )
+  );
+
+  var regionManager = new Marionette.RegionManager();
+  regionManager.addRegion('math', '#math');
+  regionManager.get('math').show(new CategoriesView(
+      { collection: categoriesCollection }
+    )
+  );
+})
+
