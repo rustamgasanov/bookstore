4fcbabaf8debeb5e022ad6fe416996d793af921d
Rustam Gasanov <rustamagasanov@gmail.com>
2015-02-05 15:56:30 +0700

    [^] refactored babysitter example

--- source_code/js/example_6_babysitter.js
+++ source_code/js/example_6_babysitter.js
@@ -1,4 +1,5 @@
 var BookStoreApp = new Marionette.Application();
+
 BookStoreApp.addRegions({
   mainRegion: '#application'
 })
@@ -15,57 +16,6 @@ var BookStoreRouter = Marionette.AppRouter.extend({
   }
 });
 
-var HelperView = Backbone.Marionette.ItemView.extend({
-  template: '#sample-template2',
-    events: {
-      'click #myButton4': 'changeColor',
-      'click #myButton5': 'logSomething'
-    },
-
-    logSomething: function (){
-      console.log('something');
-    },
-
-    changeColor: function (){
-      var hue = 'rgb(' + (Math.floor(Math.random() * 256)) +
-                   ',' + (Math.floor(Math.random() * 256)) +
-                   ',' + (Math.floor(Math.random() * 256)) + ')';
-      this.$('#dummy').css('color', hue);
-    }
-});
-
-var SampleView = Backbone.Marionette.ItemView.extend({
-    template : '#sample-template',
-
-    initialize: function() {
-      this.container = new Backbone.ChildViewContainer();
-    },
-
-    events: {
-      'click #myButton': 'addView',
-      'click #myButton2': 'callChangeColorinViews',
-      'click #myButton3': 'countViews' 
-    },
-
-    addView: function() {
-      var helperView = new HelperView();
-      helperView.render();
-      this.$el.append(helperView.el);
-      this.container.add(helperView);
-    },
-
-    callChangeColorinViews: function() {
-      this.container.each(function(view){
-          view.changeColor();
-      });
-    },
-
-    countViews: function() {
-      this.$('#existentViews').text('currently we have ' + ' ' +
-        this.container.length + ' ' + ' views in the container.');
-    }
-});
-
 BookStoreApp.addInitializer(function () {
   var controller = new BookStoreController();
   var router = new BookStoreRouter({controller: controller});
@@ -77,6 +27,62 @@ BookStoreApp.on('start', function () {
     Backbone.history.start();
   }
   console.log('Message from initialize:after method');
+});
+
+var HelperView = Backbone.Marionette.ItemView.extend({
+  template: '#sample-template-2',
+
+  ui: {
+    dummy: '#dummy'
+  },
+
+  events: {
+    'click #my-button-4': 'changeColor',
+    'click #my-button-5': 'logSomething'
+  },
+
+  logSomething: function (){
+    console.log('something');
+  },
+
+  changeColor: function (){
+    var hue = 'rgb(' + (Math.floor(Math.random() * 256)) +
+                 ',' + (Math.floor(Math.random() * 256)) +
+                 ',' + (Math.floor(Math.random() * 256)) + ')';
+    this.ui.dummy.css('color', hue);
+  }
+});
+
+var SampleView = Backbone.Marionette.ItemView.extend({
+    template: '#sample-template-1',
+
+    initialize: function() {
+      this.container = new Backbone.ChildViewContainer();
+    },
+
+    events: {
+      'click #my-button-1': 'addView',
+      'click #my-button-2': 'callChangeColorinViews',
+      'click #my-button-3': 'countViews' 
+    },
+
+    addView: function() {
+      var helperView = new HelperView();
+      helperView.render();
+      this.$el.append(helperView.el);
+      // this.container.add(helperView);
+    },
+
+    callChangeColorinViews: function() {
+      this.container.each(function(view){
+          view.changeColor();
+      });
+    },
+
+    countViews: function() {
+      this.$('#existing-views').text('currently we have ' +
+        this.container.length + ' views in the container.');
+    }
 });
 
 BookStoreApp.start();
