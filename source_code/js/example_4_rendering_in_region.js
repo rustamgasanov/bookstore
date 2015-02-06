bcb25235955f669b6f7c31f9b032f84bd7ec3688
Rustam Gasanov <rustamagasanov@gmail.com>
2015-02-06 14:20:19 +0700

    [^] simplified example 4 rendering in region

--- source_code/js/example_4_rendering_in_region.js
+++ source_code/js/example_4_rendering_in_region.js
@@ -15,6 +15,19 @@ var BookStoreRouter = Marionette.AppRouter.extend({
   }
 });
 
+BookStoreApp.addInitializer(function () {
+  var controller = new BookStoreController();
+  var router = new BookStoreRouter({controller: controller});
+  console.log('Message from the addInitializer Method');
+})
+
+BookStoreApp.on('start', function () {
+  if (Backbone.history) {
+    Backbone.history.start();
+  }
+  console.log('Message from initialize:after method');
+});
+
 var CategoryModel = Backbone.Model.extend({
   defaults: {
     name: '',
@@ -47,24 +60,12 @@ var CatalogLayoutView = Marionette.LayoutView.extend({
   }
 })
 
-BookStoreApp.addInitializer(function () {
-  var controller = new BookStoreController();
-  var router = new BookStoreRouter({controller: controller});
-  console.log('Message from the addInitializer Method');
-})
-
-BookStoreApp.on('start', function () {
-  if (Backbone.history) {
-    Backbone.history.start();
-  }
-  console.log('Message from initialize:after method');
-});
-
 BookStoreApp.start();
 
 $(function() {
   var catalogLayoutView = new CatalogLayoutView();
   BookStoreApp.mainRegion.show(catalogLayoutView);
+
   var categoriesCollection = new CategoriesCollection([
     { name: 'category1', booksOnCategory: 5 },
     { name: 'category2', booksOnCategory: 10 }
