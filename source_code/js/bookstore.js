121ad716bb3e1981fc7f944e32737297be9bed21
Rustam Gasanov <rustamagasanov@gmail.com>
2015-03-04 20:38:24 +0200

    [+] added event aggregator trigger on button click

--- source_code/js/bookstore.js
+++ source_code/js/bookstore.js
@@ -1,6 +1,18 @@
 var BookStoreApp = new Marionette.Application();
+var vent = new Backbone.Wreqr.EventAggregator();
 
 var BookStoreController = Marionette.Controller.extend({
+  initialize: function() {
+    var self = this;
+    BookStoreApp.vent.on('itemAdded', function(model) {
+      self.addBook(model)
+    });
+  },
+
+  addBook: function(model) {
+    console.log('adding a book');
+  },
+
   displayBooks: function() {
     console.log('I will display books...');
   }
@@ -38,6 +50,13 @@ var BookCollection = Backbone.Collection.extend({
 
 var BookItemView = Marionette.ItemView.extend({
   template: '#books-template',
+  events: {
+    'click .btn-primary': 'addItem',
+  },
+
+  addItem: function() {
+    BookStoreApp.vent.trigger('itemAdded', 'stub');
+  }
 })
 
 BookStoreApp.start();
@@ -48,14 +67,6 @@ $(function() {
     { id: 2, name: 'Second' },
     { id: 3, name: 'Third' }
   ]);
-
-
-  var vent = new Backbone.Wreqr.EventAggregator();
-  BookStoreApp.vent.on("my_event", function(){
-    console.log("im logging a message");
-  });
-  BookStoreApp.vent.trigger("my_event");
-
 
   var booksView = new BookItemView({ collection: booksCollection, el: '#application' })
   booksView.render();
