ec80d2110aa0e922704d9288ca28eb24981323a3
Rustam Gasanov <rustamagasanov@gmail.com>
2015-03-17 20:09:16 +0300

    [+] added handlebars and usage example

--- source_code/js/bookstore.js
+++ source_code/js/bookstore.js
@@ -1,40 +1,22 @@
 var BookStoreApp = new Marionette.Application();
-var vent = new Backbone.Wreqr.EventAggregator();
 
 var BookStoreController = Marionette.Controller.extend({
-  initialize: function() {
-    var self = this;
-    BookStoreApp.vent.on('itemAdded', function(model) {
-      self.addBook(model)
-    });
-  },
-
-  addBook: function(model) {
-    console.log('adding a book');
-  },
-
-  displayBooks: function() {
-    console.log('I will display books...');
-  }
+  displayBooks: function() { }
 });
 
 var BookStoreRouter = Marionette.AppRouter.extend({
-  appRoutes: {
-    '': 'displayBooks'
-  }
+  appRoutes: { '': 'displayBooks' }
 });
 
 BookStoreApp.addInitializer(function () {
   var controller = new BookStoreController();
   var router = new BookStoreRouter({controller: controller});
-  console.log('Message from the addInitializer Method');
 })
 
 BookStoreApp.on('start', function () {
   if (Backbone.history) {
     Backbone.history.start();
   }
-  console.log('Message from initialize:after method');
 });
 
 var BookModel = Backbone.Model.extend({
@@ -44,31 +26,17 @@ var BookModel = Backbone.Model.extend({
   }
 });
 
-var BookCollection = Backbone.Collection.extend({
-  model: BookModel
-});
-
 var BookItemView = Marionette.ItemView.extend({
-  template: '#books-template',
-  events: {
-    'click .btn-primary': 'addItem',
-  },
-
-  addItem: function() {
-    BookStoreApp.vent.trigger('itemAdded', 'stub');
-  }
+  el: '#application',
+  // template: '#books-template'
+  template : Handlebars.compile($('#books-template').html())
 })
 
 BookStoreApp.start();
 
 $(function() {
-  var booksCollection = new BookCollection([
-    { id: 1, name: 'First' },
-    { id: 2, name: 'Second' },
-    { id: 3, name: 'Third' }
-  ]);
-
-  var booksView = new BookItemView({ collection: booksCollection, el: '#application' })
+  var book = new BookModel({ id: 1, name: 'First Book' });
+  var booksView = new BookItemView({ model: book })
   booksView.render();
 })
 
